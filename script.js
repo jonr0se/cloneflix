c1262a7ad0d392da769d0b6c6221b5ff60f347db
Jonathan <jonrose@protonmail.com>
2025-06-26 16:48:21 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Optional interactivity or animations
+document.querySelectorAll('.row img').forEach(img => {
+  img.addEventListener('click', () => {
+    alert('This is a demo Netflix clone. No video available.');
+  });
+});
